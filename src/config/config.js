f049d0e239d25418cdd7129393419dd91e9a7440
only back for cs2 <39854659+undercoverbtw@users.noreply.github.com>
2024-09-09 16:24:49 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -6,10 +6,10 @@ module.exports = {
         useHttps: false, // Default: false
     },
     proxy: {
-        scrape: true, // Default: 'false' | Set to true to enable proxy scraping | false use proxies.txt
+        scrape: false, // Default: 'false' | Set to true to enable proxy scraping | false use proxies.txt
         protocol: 'http', // Default: 'http' | Proxy protocol, can be 'http', 'https', 'socks4', 'socks5'
         timeout: 5000
     },
     botsName: 'NELBOTS',
-    botsAmount: 500
+    botsAmount: 5
 }
